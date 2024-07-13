81bfab692c39bc224b537121ed3d98e4712c4b93
Louis Lee <louislee92@163.com>
2024-07-13 20:03:56 +0800

    Create AntvX6Graph.js

---
+++ src/views/antv-x6/AntvX6Graph.js
@@ -0,0 +1,20 @@
+
+import { Graph } from '@antv/x6'
+
+const AntvX6Graph = function(opt) {
+
+  const graph = new Graph({
+    container: opt.container,
+    width: 800,
+    height: 600,
+    background: {
+      color: '#F2F7FA',
+    },
+  });
+
+
+}
+
+
+export default AntvX6Graph
+
